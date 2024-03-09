f9eb430ccfaf4e7293475c018df533836cf22ff0
khanatal92 <akhan126@insite.4cd.edu>
2024-03-08 21:33:54 -0800

    commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include    <iostream>
 using namespace std;
 
-int main()
-{
-   
-   /* Complete your code */
+int main() {
+   int rows;
+
+   // Ask user for input
+   cout << "Enter the number of rows: ";
+   cin >> rows;
+
+   // Check if the input is valid
+   if (rows <= 0) {
+      cout << "Invalid input. Please enter a positive integer." << endl;
+   }
   
+   // Print the pattern{}
 }
